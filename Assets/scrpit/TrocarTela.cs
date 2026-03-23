e219a5c3d4a3b9b9ea28ca4d68e3ef82cdc4c67f
jessicasilvasantosf-dev <jessicasilvasantosf@gmail.com>
2026-03-23 09:30:32 -0300

    apresentação completo

--- Assets/scrpit/TrocarTela.cs
+++ Assets/scrpit/TrocarTela.cs
@@ -2,13 +2,36 @@ using UnityEngine;
 
 public class TrocarTela : MonoBehaviour
 {
-    public GameObject canvas1;
-    public GameObject canvas2;
+    public GameObject canva;
+    public GameObject canva2;
+    public GameObject canva3;
 
     public void Trocar()
     {
          Debug.Log("CLICOU");
-        canvas1.SetActive(false);
-        canvas2.SetActive(true);
+        canva.SetActive(false);
+        canva2.SetActive(true);
+        canva3.SetActive(false);
+    }
+    public void Trocar2()
+    {
+        Debug.Log("CLICOU");
+        canva.SetActive(false);
+        canva2.SetActive(false);
+        canva3.SetActive(true);
+    }
+    public void Voltar()
+    {
+        Debug.Log("CLICOU");
+        canva.SetActive(true);
+        canva2.SetActive(false);
+        canva3.SetActive(false);
+    }
+    public void Voltar2()
+    {
+        Debug.Log("CLICOU");
+        canva.SetActive(false);
+        canva2.SetActive(true);
+        canva3.SetActive(false);
     }
 }
