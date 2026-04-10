10b9e7e8796396530f88233c7b32c478273413e9
jessicasilvasantosf-dev <jessicasilvasantosf@gmail.com>
2026-04-10 10:47:24 -0300

    introdução-atualizada

--- Assets/scrpit/TrocarTela.cs
+++ Assets/scrpit/TrocarTela.cs
@@ -1,16 +1,88 @@
 using UnityEngine;
 
-public class TrocarTela : MonoBehaviour
+public class GerenciadorTelas : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject canva1;
+    public GameObject canva2;
+    public GameObject canva3;
+    public GameObject canva4;
+    public GameObject canva5;
+    public GameObject canva6;
+    public GameObject comecar;
+
+    public void MostrarTela1()
     {
-        
+        canva1.SetActive(true);
+        canva2.SetActive(false);
+        canva3.SetActive(false);
+        canva4.SetActive(false);
+        canva5.SetActive(false);
+        canva6.SetActive(false);
+        comecar.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MostrarTela2()
     {
-        
+        canva1.SetActive(false);
+        canva2.SetActive(true);
+        canva3.SetActive(false);
+        canva4.SetActive(false);
+        canva5.SetActive(false);
+        canva6.SetActive(false);
+        comecar.SetActive(false);
     }
-}
+
+    public void MostrarTela3()
+    {
+        canva1.SetActive(false);
+        canva2.SetActive(false);
+        canva3.SetActive(true);
+        canva4.SetActive(false);
+        canva5.SetActive(false);
+        canva6.SetActive(false);
+        comecar.SetActive(false);
+    }
+
+    public void MostrarTela4()
+    {
+        canva1.SetActive(false);
+        canva2.SetActive(false);
+        canva3.SetActive(false);
+        canva4.SetActive(true);
+        canva5.SetActive(false);
+        canva6.SetActive(false);
+        comecar.SetActive(false);
+    }
+
+    public void MostrarTela5()
+    {
+        canva1.SetActive(false);
+        canva2.SetActive(false);
+        canva3.SetActive(false);
+        canva4.SetActive(false);
+        canva5.SetActive(true);
+        canva6.SetActive(false);
+        comecar.SetActive(false);
+    }
+    public void MostrarTela6()
+    {
+        canva1.SetActive(false);
+        canva2.SetActive(false);
+        canva3.SetActive(false);
+        canva4.SetActive(false);
+        canva5.SetActive(false);
+        canva6.SetActive(true);
+        comecar.SetActive(false);
+    }
+
+    public void Inciar()
+    {
+        canva1.SetActive(false);
+        canva2.SetActive(false);
+        canva3.SetActive(false);
+        canva4.SetActive(false);
+        canva5.SetActive(false);
+        canva6.SetActive(false);
+        comecar.SetActive(true);
+    }
+ }
