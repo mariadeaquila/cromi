f44e0433d5a8fc38405ac84042f03cb48ddba9c1
consonlee <sabrinayunalee.2006@gmail.com>
2026-04-13 11:53:03 -0300

    configuracao

--- Assets/scrpit/SwitchToggle.cs
+++ Assets/scrpit/SwitchToggle.cs
@@ -1,57 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 
 public class ToggleSwitch : MonoBehaviour
 {
-    public Toggle toggle;
-
-    public RectTransform handle;
-    public Image background;
-
-    public Vector2 onPosition;
-    public Vector2 offPosition;
-
-    public Color onColor;
-    public Color offColor;
-
-    public float speed = 5f;
+    public Image On;
+    public Image Off;
+    int index;
 
     void Start()
     {
-        toggle.onValueChanged.AddListener(OnSwitchChanged);
-
-        // Atualiza estado inicial
-        OnSwitchChanged(toggle.isOn);
+        
     }
 
-    void OnSwitchChanged(bool isOn)
+    public void ON()
     {
-        StopAllCoroutines();
-        StartCoroutine(AnimateSwitch(isOn));
+        index = 1;
+        Off.gameObject.SetActive(true);
+        On.gameObject.SetActive(false);
     }
 
-    IEnumerator AnimateSwitch(bool isOn)
+    public void OFF()
     {
-        Vector2 startPos = handle.anchoredPosition;
-        Vector2 endPos = isOn ? onPosition : offPosition;
-
-        Color startColor = background.color;
-        Color endColor = isOn ? onColor : offColor;
-
-        float t = 0;
-
-        while (t < 1)
-        {
-            t += Time.deltaTime * speed;
-
-            handle.anchoredPosition = Vector2.Lerp(startPos, endPos, t);
-            background.color = Color.Lerp(startColor, endColor, t);
-
-            yield return null;
-        }
-
-        handle.anchoredPosition = endPos;
-        background.color = endColor;
+        index = 0;
+        On.gameObject.SetActive(true);
+        Off.gameObject.SetActive(false);
     }
 }
