3f7be09c894a93f8a86c55168042ab46c86dd0bc
consonlee <sabrinayunalee.2006@gmail.com>
2026-04-15 15:04:42 -0300

    Revert "o"
    This reverts commit c9dd2312dc8526010a20444774152ebbee142c33.

---
+++ Assets/ble2/Scripts/InicialVerificaPermissoes/PermissaoBluetooth.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.SceneManagement;
+
+public class PermissaoBluetooth : MonoBehaviour
+{
+    [Header("Cenas")]
+    public string cenaBluetooth = "CenaBluetooth";
+    public string cenaPermissaoNegada = "CenaPermissaoNegada";
+
+    private const string BluetoothScan = "android.permission.BLUETOOTH_SCAN";
+    private const string BluetoothConnect = "android.permission.BLUETOOTH_CONNECT";
+
+    private bool cenaJaFoiTrocada = false;
+    private int permissoesConcedidas = 0;
+
+    void Start()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        if (TemTodasPermissoes())
+        {
+            IrParaCenaBluetooth();
+            return;
+        }
+
+        SolicitarPermissoes();
+#else
+        IrParaCenaBluetooth();
+#endif
+    }
+
+    private void SolicitarPermissoes()
+    {
+        PermissionCallbacks callbacks = new PermissionCallbacks();
+
+        callbacks.PermissionGranted += OnPermissionGranted;
+        callbacks.PermissionDenied += OnPermissionDenied;
+        callbacks.PermissionDeniedAndDontAskAgain += OnPermissionDeniedAndDontAskAgain;
+
+        Permission.RequestUserPermissions(
+            new string[] { BluetoothScan, BluetoothConnect },
+            callbacks
+        );
+    }
+
+    private bool TemTodasPermissoes()
+    {
+        return Permission.HasUserAuthorizedPermission(BluetoothScan) &&
+               Permission.HasUserAuthorizedPermission(BluetoothConnect);
+    }
+
+    private void OnPermissionGranted(string permissionName)
+    {
+        if (cenaJaFoiTrocada)
+            return;
+
+        permissoesConcedidas++;
+
+        if (TemTodasPermissoes())
+        {
+            IrParaCenaBluetooth();
+        }
+    }
+
+    private void OnPermissionDenied(string permissionName)
+    {
+        if (cenaJaFoiTrocada)
+            return;
+
+        IrParaCenaPermissaoNegada();
+    }
+
+    private void OnPermissionDeniedAndDontAskAgain(string permissionName)
+    {
+        if (cenaJaFoiTrocada)
+            return;
+
+        IrParaCenaPermissaoNegada();
+    }
+
+    private void IrParaCenaBluetooth()
+    {
+        if (cenaJaFoiTrocada)
+            return;
+
+        cenaJaFoiTrocada = true;
+       // SceneManager.LoadScene(cenaBluetooth);
+    }
+
+    private void IrParaCenaPermissaoNegada()
+    {
+        if (cenaJaFoiTrocada)
+            return;
+
+        cenaJaFoiTrocada = true;
+        //SceneManager.LoadScene(cenaPermissaoNegada);
+    }
+}
